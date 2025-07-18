fde59d3487209b88125f892e597cd8eb3ec5cdea
santoshk66 <santosh.k66@gmail.com>
2025-07-18 17:41:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import fs from 'fs/promises';
-import cors from 'cors';
-import path from 'path';
+const express = require('express');
+const fs = require('fs').promises;
+const cors = require('cors');
+const path = require('path');
 
 const app = express();
 app.use(cors());
